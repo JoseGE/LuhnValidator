6fcadd4c55be6c92f5fe0cee459905ba76a97a15
Carlos Herasme <rcherasmeestrada@gmail.com>
2019-06-21 21:03:41 -0400

    Le coulle le crayon

--- LuhnValidator/IdCardvalidator.cs
+++ LuhnValidator/IdCardvalidator.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace LuhnValidator
 {
     public class IdCardValidator
     {
+        private readonly Dictionary<string, int> countries;
+
+        public IdCardValidator()
+        {
+            countries = new Dictionary<string, int>()
+            {
+                { "DO", 11 },
+                { "NI", 11 },
+                { "US", 11 },
+                { "FR", 9 },
+            };
+        }
+
         public bool ValidateIdCard(string card)
         {
             int sum = Convert.ToInt32(card.Substring(card.Length - 1));
@@ -26,6 +38,42 @@ namespace LuhnValidator
 
             return sum % 10 == 0;
         }
+
+        public bool ValidateIdCard(string cardNumber, string countryISO)
+        {
+            var isValidCountryISO = countries.TryGetValue(countryISO, out int idCardLenght);
+
+            if (!isValidCountryISO)
+                return false;
+
+            string card = default;
+
+            foreach (var character in cardNumber)
+                if (int.TryParse(character.ToString(), out int isNumber))
+                    card += character.ToString();
+
+            if (card.Length != idCardLenght)
+                return false;
+
+            int sum = Convert.ToInt32(card.Substring(card.Length - 1));
+            int digits = card.Length;
+            int parity = digits % 2;
+
+            for (int i = 0; i < digits - 1; i++)
+            {
+                int digit = Convert.ToInt32(card[i].ToString());
+
+                if (i % 2 == parity)
+                    digit = digit * 2;
+                if (digit > 9)
+                    digit = digit - 9;
+
+                sum += digit;
+            }
+
+            return sum % 10 == 0;
+        }
+
 
         public bool ValidateCreditCard(string cardNumbers)
         {
